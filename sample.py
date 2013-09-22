b69a2a55149d10606743ac4425b21e10921fd655
Ragesh Krishna <ragesh@rageshkrishna.com>
2013-09-22 11:34:29 +0530

    Initial commit

---
+++ sample.py
@@ -0,0 +1,6 @@
+import os, memcache
+
+memcached_port = os.environ.get("SHIPPABLE_MEMCACHED_PORT")
+memcached = memcache.Client(["127.0.0.1:{0}".format(memcached_port))
+memcached.set("name", "Shippable Memcached Demo")
+print memcached.get("name")
