8aebdeb46ca99bc899d917d00148340b88b8cf32
Devashish <devashish.86@gmail.com>
2013-10-29 18:05:49 +0530

    using default memcache port

--- sample.py
+++ sample.py
@@ -1,6 +1,7 @@
 import os, memcache
 
-memcached_port = os.environ.get("SHIPPABLE_MEMCACHED_PORT")
+# memcached_port = os.environ.get("SHIPPABLE_MEMCACHED_PORT")
+memcached_port = 11211
 memcached = memcache.Client(["127.0.0.1:{0}".format(memcached_port)])
 memcached.set("name", "Shippable Memcached Demo")
 print memcached.get("name")
